8e54622205f42ef2d4894e2f01e4ccbb2e046a2c
jlgoldb2 <jlgoldb2@asu.edu>
2014-10-02 20:06:02 -0700

    updated setup

--- setup.py
+++ setup.py
@@ -4,14 +4,17 @@ project = "flask-simple-alchemy"
 
 setup(
     name=project,
-    version='0.6.0a',
+    version='0.1.0',
+    url='https://github.com/jlgoldb2/flask-simple-alchemy',
     description='A Simplification of SQLAlchemy\'s declarative using '\
         + ' Flask-SQLAlchemy',
     author='Jason Goldberger',
+    license='MIT',
     author_email='jlgoldb2@asu.edu',
     packages=["simple_sqlalchemy"],
     include_package_data=True,
     zip_safe=False,
+    platforms='any',
     install_requires=[
         'Flask',
         'Flask-SQLAlchemy',
--- simple_sqlalchemy/config.py
+++
@@ -1,3 +0,0 @@
-SQLALCHEMY_DATABASE_URI ='sqlite:///example.db'
-DEBUG = True
-SECRET_KEY = 'development key'
