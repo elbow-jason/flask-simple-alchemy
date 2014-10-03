d24726103bff790860aa7647f833e1471c3747cd
jlgoldb2 <jlgoldb2@asu.edu>
2014-10-02 20:06:35 -0700

    moved app to examples

---
+++ tests/test_example_app.py
@@ -0,0 +1,17 @@
+
+from simple_sqlalchemy.example_app import *
+import simple_sqlalchemy.config as cfg
+
+
+def test_create_app():
+    app = create_app()
+    assert app.__dict__.has_key('url_map')
+
+def test_config_app():
+    app = Flask(__name__)
+    config_app(app, cfg)
+    assert app.config.has_key('SQLALCHEMY_DATABASE_URI')
+    assert app.config.has_key('DEBUG')
+    assert app.config.has_key('SECRET_KEY')
+    
+    assert app.config['SQLALCHEMY_DATABASE_URI'] =='sqlite:///example.db'
