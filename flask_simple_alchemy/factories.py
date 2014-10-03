a2cf8e96de24d16768ca3338aa80f3b8b32ad08b
jlgoldb2 <jlgoldb2@asu.edu>
2014-10-02 20:41:57 -0700

    added init to relationship factories

--- flask_simple_alchemy/factories.py
+++ flask_simple_alchemy/factories.py
@@ -1,11 +1,28 @@
+from flask.ext.sqlalchemy import SQLAlchemy
+
 
 class RelationshipFactories(object):
 
+    def __init__(self, db):
+        """
+        I initialize the RelationshipFactories's instance.
+        I expect an instance of the SQLAlchemy object.
+        Constructor.
 
-    def id_num_relation_factory(column_name, **kwargs):
+            :param db:
+                Flask-SQLAlchemy database object
+        """
+        if not isinstance(db, SQLAlchemy):
+            raise Exception('The RelationshipFactories object\
+                requires/expects an instance of the SQLAlchemy object.')
+        self.db = db
+
+
+
+    def id_num_relation_factory(self, column_name, **kwargs):
         foreign_key  = kwargs.get('foreign_key', 'id')
         nullable     = kwargs.get('nullable', False)
-        fk_data_type = kwargs.get('column_type', db.Integer)
+        fk_data_type = kwargs.get('column_type', self.db.Integer)
         fk           = '{}.{}'.format(column_name, foreign_key)
         def declare_id(name):
             @declared_attr
---
+++ tests/test_factories.py
@@ -0,0 +1,5 @@
+
+from flask_simple_alchemy import RelationshipFactories
+
+def test_RelationshipFactories_init():
+    pass
